7f7c5cc4b408f6708e21bd45c54365ef110147fa
dingding <2819742339@qq.com>
2023-08-14 19:47:37 +0800

    小丁，feat:init ddd project

---
+++ chatbot-api-infrastructure/src/main/java/cn/bugstack/chatbot/api/infrastructure/package-info.java
@@ -0,0 +1,8 @@
+package cn.bugstack.chatbot.api.infrastructure;
+
+
+/**
+*
+*基础层，用于连库，连接数据库
+*
+* */
---
+++ chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
@@ -0,0 +1,7 @@
+package cn.bugstack.chatbot.api.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApiTest {
+}
